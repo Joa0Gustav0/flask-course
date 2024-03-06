63fa240ffa2489628bab9144633c5587de8d594d
Joao <dev.joaogustavo.dev@gmail.com>
2024-03-05 23:16:06 -0300

    trained template data inserting using database data

--- training-project/app.py
+++ training-project/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__);
 
 from data import Crud;
+
 
 @app.route("/")
 def index() :
@@ -11,5 +12,6 @@ def index() :
 def market() :
   return render_template(
     "market.html", 
-    items=Crud().executeCrudAction("read", "SELECT * FROM marketitems;")
+    items=Crud().executeCrudAction("read", "SELECT * FROM marketitems;"),
+    styles=[url_for("market")]
   );
