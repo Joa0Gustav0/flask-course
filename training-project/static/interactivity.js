4de72203832e7dd64c9c6a283e449bc2146e8de5
Joao <dev.joaogustavo.dev@gmail.com>
2024-03-19 21:14:44 -0300

    created method for creating an notification element or remove an existent one

--- training-project/static/interactivity.js
+++ training-project/static/interactivity.js
@@ -40,3 +40,62 @@ class ImageLoader {
   }
 }
 
+class AppNotification {
+  static unities = 0;
+
+  constructor(message, keywords) {
+    AppNotification.unities++;
+    this.keywords = keywords;
+    this.message = this.highlightKeywords(message);
+
+    this.createNotificationElement();
+  }
+
+  highlightKeywords(entryMessage) {
+    let outputMessage = "";
+
+    entryMessage = entryMessage.split(" ");
+    entryMessage.forEach((word) => {
+      outputMessage += (this.keywords.includes(word)
+        ? `<span class='highlighted-text'>${word}</span>`
+        : word) + " ";
+    });
+
+    return outputMessage;
+  }
+
+  createNotificationElement() {
+    const NOTIFICATION_MODEL = `
+    <span class="notification-popup-container" id="notification-${AppNotification.unities}">
+      <h1 class="notification-popup-container__headline">
+        🦄 Hey! Notificação!
+      </h1>
+      <p class="notification-popup-container__message-text">
+        ${this.message}
+      </p>
+      <img src="/static/media/unicorn-notification-icon.png" alt="Ícone representativo de um unicórnio de brinquedo" class="notification-popup-container__floating-icon" />
+      <button 
+        class="notification-popup-container__close-button"
+        onclick="AppNotification.deleteNotificationElement(${AppNotification.unities})"
+      >
+        <ion-icon name="close"></ion-icon>
+      </button>
+    </span>
+    `;
+
+    document.body.innerHTML += NOTIFICATION_MODEL;
+  }
+
+  static deleteNotificationElement(elementIDNumber) {
+    let targetElement = document.getElementById(
+      "notification-" + elementIDNumber
+    );
+
+    targetElement.classList.add("out");
+    setTimeout(() => targetElement.remove(), 1250);
+  }
+}
+new AppNotification(
+  "A sua conta foi criada com sucesso. Faça login e conheça o que o UniMarket tem a te oferecer!",
+  ["conta", "criada", "com", "sucesso.", "UniMarket"]
+);
